7f30140eb423f075f5fc14d0dba2e0de6dc09df1
Steven Moore <steve.natgate@gmail.com>
2023-07-10 22:20:44 +1000

    API: Fixed stack attributes not being used

--- dotnet/NaturalFacade.ApiServices/LayoutConfig/RawXml/BranchElementHandlers/StackElementHandler.cs
+++ dotnet/NaturalFacade.ApiServices/LayoutConfig/RawXml/BranchElementHandlers/StackElementHandler.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -29,6 +29,41 @@ namespace NaturalFacade.LayoutConfig.RawXml
             };
         }
 
+        /// <summary>Gets the parent-stack specific attributes and applies them to the child object.</summary>
+        private void ApplyStackChildAttributes(ITagAttributes attributes, Dictionary<string, object> data)
+        {
+            // Get attributes
+            Raw.RawLayoutConfigElementStackHAlignment hAlign = attributes.GetNullableEnum<Raw.RawLayoutConfigElementStackHAlignment>("stack.HAlign") ?? Raw.RawLayoutConfigElementStackHAlignment.Fill;
+            Raw.RawLayoutConfigElementStackVAlignment vAlign = attributes.GetNullableEnum<Raw.RawLayoutConfigElementStackVAlignment>("stack.VAlign") ?? Raw.RawLayoutConfigElementStackVAlignment.Fill;
+            long widthPixels = attributes.GetNullableLong("stack.WidthPixels") ?? 0;
+            long heightPixels = attributes.GetNullableLong("stack.HeightPixels") ?? 0;
+            long margin = attributes.GetNullableLong("stack.Margin") ?? 0;
+            long marginHorizontal = attributes.GetNullableLong("stack.MarginHorizontal") ?? margin;
+            long marginVertical = attributes.GetNullableLong("stack.MarginVertical") ?? margin;
+            long marginLeft = attributes.GetNullableLong("stack.MarginLeft") ?? marginHorizontal;
+            long marginRight = attributes.GetNullableLong("stack.MarginRight") ?? marginHorizontal;
+            long marginTop = attributes.GetNullableLong("stack.MarginTop") ?? marginVertical;
+            long marginBottom = attributes.GetNullableLong("stack.MarginBottom") ?? marginVertical;
+
+            // Apply attributes
+            if (hAlign != Raw.RawLayoutConfigElementStackHAlignment.Fill)
+                data.Add("halign", hAlign.ToString());
+            if (vAlign != Raw.RawLayoutConfigElementStackVAlignment.Fill)
+                data.Add("valign", vAlign.ToString());
+            if (widthPixels != 0)
+                data.Add("width", widthPixels);
+            if (heightPixels != 0)
+                data.Add("height", heightPixels);
+            if (marginLeft != 0)
+                data.Add("stackMarginLeft", marginLeft);
+            if (marginRight != 0)
+                data.Add("stackMarginRight", marginRight);
+            if (marginTop != 0)
+                data.Add("stackMarginTop", marginTop);
+            if (marginBottom != 0)
+                data.Add("stackMarginBottom", marginBottom);
+        }
+
         #endregion
 
         #region IBranchElementHandler implementation
@@ -47,6 +82,7 @@ namespace NaturalFacade.LayoutConfig.RawXml
                 IBranchElementHandler branchElementHandler = RawXmlElementFactory.CheckBranchTag(m_tracking, tagName, attributes);
                 if (branchElementHandler != null)
                 {
+                    ApplyStackChildAttributes(attributes, branchElementHandler.Data);
                     m_childList.Add(branchElementHandler.Data);
                     return branchElementHandler;
                 }
@@ -55,6 +91,7 @@ namespace NaturalFacade.LayoutConfig.RawXml
                 Dictionary<string, object> leafData = RawXmlElementFactory.CheckLeafTag(m_tracking, tagName, attributes);
                 if (leafData != null)
                 {
+                    ApplyStackChildAttributes(attributes, leafData);
                     m_childList.Add(leafData);
                     return null;
                 }
